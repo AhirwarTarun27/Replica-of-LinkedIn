b0bca176f50c640d79f98052b80a64cc3bd79ee8
Tarun Chandraprakash Ahirwar <codertarun328@gmail.com>
2022-03-27 04:20:47 +0530

    mid Feed section started and initiated the top ShareBox

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,35 @@
 import styled from "styled-components";
 
 const Main = (props) => {
-  return <Container>Main</Container>;
+  return (
+    <Container>
+      <ShareBox>Share</ShareBox>
+    </Container>
+  );
 };
 
 const Container = styled.div`
   grid-area: main;
+`;
+
+const CommonCard = styled.div`
+  text-align: center;
+  overflow: hidden;
+  margin-bottom: 8px;
+  background-color: #fff;
+  border-radius: 5px;
+  position: relative;
+  border: none;
+  box-shadow: 0 0 0 1px rgb(0 0 0 / 15%), 0 0 0 rgb(0 0 0 / 20%);
+  padding: 12px;
+`;
+
+const ShareBox = styled(CommonCard)`
+  display: flex;
+  flex-direction: column;
+  color: #958b7b;
+  margin: 0 0 8px;
+  background: white;
 `;
 
 export default Main;
