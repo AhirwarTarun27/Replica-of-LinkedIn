c4cfd3025711659cd7344f5df034a90ed24d5722
Tarun Chandraprakash Ahirwar <codertarun328@gmail.com>
2022-03-27 03:12:24 +0530

    left card is in progress

--- src/components/LeftSide.js
+++ src/components/LeftSide.js
@@ -14,6 +14,21 @@ const Leftside = (props) => {
             <AddPhotoText>Add a Photo</AddPhotoText>
           </a>
         </UserInfo>
+        <Widget>
+          <a>
+            <div>
+              <span>Connections</span>
+              <span>Grow your network</span>
+            </div>
+            <img src="/images/widget-icon.svg" alt="" />
+          </a>
+        </Widget>
+        <Item>
+          <span>
+            <img src="/images/item-icon.svg" alt="" />
+            My Items
+          </span>
+        </Item>
       </ArtCard>
     </Container>
   );
@@ -64,8 +79,78 @@ const Photo = styled.div`
   border-radius: 50%;
 `;
 
-const Link = styled.div``;
+const Link = styled.div`
+  font-size: 16px;
+  line-height: 1.5;
+  color: rgba(0, 0, 0, 0.9);
+  font-weight: 600;
+`;
 
-const AddPhotoText = styled.div``;
+const AddPhotoText = styled.div`
+  color: #0a66c2;
+  margin-top: 4px;
+  font-size: 12px;
+  line-height: 1.33;
+  font-weight: 500;
+`;
+
+const Widget = styled.div`
+  border-bottom: 1px solid rgba(0, 0, 0, 0.15);
+  padding-top: 12px;
+  padding-bottom: 12px;
+
+  & > a {
+    text-decoration: none;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 4px 12px;
+  }
+
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.08);
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+    text-align: left;
+    span {
+      font-size: 12px;
+      line-height: 1.333;
+
+      &:first-child {
+        color: rgba(0, 0, 0, 0.6);
+      }
+      &:nth-child(2) {
+        color: rgba(0, 0, 0, 1);
+      }
+    }
+  }
+
+  svg {
+    color: rgba(0, 0, 0, 1);
+  }
+`;
+
+const Item = styled.a`
+  border-color: rgba(0, 0, 0, 0.8);
+  text-align: left;
+  padding: 12px;
+  font-size: 12px;
+  display: block;
+  span {
+    display: flex;
+    align-items: center;
+    color: rgba(0, 0, 0, 0.6);
+    svg {
+      color: rgba(0, 0, 0, 0.08);
+    }
+  }
+
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.08);
+  }
+`;
 
 export default Leftside;
