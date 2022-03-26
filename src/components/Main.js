9433988b2728ad7b1a5d158d07317669c3e2f8a0
Tarun Chandraprakash Ahirwar <codertarun328@gmail.com>
2022-03-27 05:03:56 +0530

    mid shareBox completed

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,31 @@ import styled from "styled-components";
 const Main = (props) => {
   return (
     <Container>
-      <ShareBox>Share</ShareBox>
+      <ShareBox>
+        Share
+        <div>
+          <img src="/images/user.svg" alt="" />
+          <button>Start a post</button>
+        </div>
+        <div>
+          <button>
+            <img src="/images/photo-icon.svg" alt="" />
+            <span>Photo</span>
+          </button>
+          <button>
+            <img src="/images/video-icon.svg" alt="" />
+            <span>Video</span>
+          </button>
+          <button>
+            <img src="/images/job-icon.svg" alt="" />
+            <span>Job</span>
+          </button>
+          <button>
+            <img src="/images/article-icon.svg" alt="" />
+            <span>Write article</span>
+          </button>
+        </div>
+      </ShareBox>
     </Container>
   );
 };
@@ -30,6 +54,53 @@ const ShareBox = styled(CommonCard)`
   color: #958b7b;
   margin: 0 0 8px;
   background: white;
+  div {
+    button {
+      outline: none;
+      color: rgba(0, 0, 0.6);
+      font-size: 14px;
+      line-height: 48px;
+      min-height: 48px;
+      background: transparent;
+      border: none;
+      display: flex;
+      align-items: center;
+      font-weight: 600;
+    }
+    &:first-child {
+      display: flex;
+      align-items: center;
+      padding: 8px 16px 0px 16px;
+      img {
+        width: 48px;
+        border-radius: 50%;
+        margin-right: 8px;
+      }
+      button {
+        margin: 4px 0;
+        flex-grow: 1;
+        border-radius: 35px;
+        padding-left: 16px;
+        border: 1px solid rgba(0, 0, 0, 0.15);
+        background-color: white;
+        text-align: left;
+      }
+    }
+    &:nth-child(2) {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-around;
+      padding-bottom: 4px;
+      button {
+        img {
+          margin: 0 4px 0 -2px;
+        }
+        span {
+          color: #958b7b;
+        }
+      }
+    }
+  }
 `;
 
 export default Main;
