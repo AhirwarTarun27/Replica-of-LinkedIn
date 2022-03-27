ed28a9ff6e6bca129008715c81dc19fcee251331
Tarun Chandraprakash Ahirwar <codertarun328@gmail.com>
2022-03-27 16:03:43 +0530

    google auth added

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { connect } from "react-redux";
+import { signInAPI } from "../actions";
 
 const Login = (props) => {
   return (
@@ -19,7 +20,7 @@ const Login = (props) => {
           <img src="/images/hero-login.svg" alt="" />
         </Hero>
         <Form>
-          <Google>
+          <Google onClick={() => props.signIn()}>
             <img src="/images/google.svg" alt="" />
             Sign in with Google
           </Google>
@@ -163,6 +164,8 @@ const mapStateToProps = (state) => {
   return {};
 };
 
-const mapDispatchToProps = (dispatch) => ({});
+const mapDispatchToProps = (dispatch) => ({
+  signIn: () => dispatch(signInAPI()),
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(Login);
